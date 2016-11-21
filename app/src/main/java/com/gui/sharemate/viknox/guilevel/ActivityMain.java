d7280e2273722ec2ad03f2982b27d00981ab3882
DESKTOP-QGR187P\x230 <viknox1912@gmail.com>
2016-11-21 17:21:08 +0100

    Main navigation added, Base of Custom Swipe Tabs added

--- app/src/main/java/com/gui/sharemate/viknox/guilevel/ActivityMain.java
+++ app/src/main/java/com/gui/sharemate/viknox/guilevel/ActivityMain.java
@@ -9,45 +9,72 @@ import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class ActivityMain extends FragmentActivity {
-    PagerTitleStrip mTitleStrip;
+public class ActivityMain extends AppCompatActivity {
+    SlidingTabLayout mSlidingBar;
     ViewPager mViewPager = null;
+    Toolbar mToolbar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
-        mViewPager = (ViewPager)findViewById(R.id.vp_viewPager) ;
-        mTitleStrip =(PagerTitleStrip)findViewById(R.id.titleStrip);
-        mTitleStrip.inflate(this,R.layout.tab_navigation,mViewPager);
-        FragmentManager fm = getSupportFragmentManager();
-        mViewPager.setAdapter(new MyPageAdapter(fm));
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        mViewPager = (ViewPager) findViewById(R.id.vp_viewPager);
+        mViewPager.setAdapter(new MyPageAdapter(getSupportFragmentManager()));
+        mSlidingBar = (SlidingTabLayout) findViewById(R.id.tabs);
+        mSlidingBar.setViewPager(mViewPager);
+
+
     }
 
-    private class MyPageAdapter  extends FragmentPagerAdapter{
+    private class MyPageAdapter extends FragmentPagerAdapter {
+        String[] tabs;
+
         public MyPageAdapter(FragmentManager fm) {
             super(fm);
+            tabs = getResources().getStringArray(R.array.tabs);
         }
 
         @Override
         public Fragment getItem(int position) {
             Fragment fragment = null;
-            if (position==0){
-                fragment = new FragmentSettings();
+            if (position == 0) {
+                fragment = new FragmentHome();
 
             }
-            if (position==1){
-                fragment = new FragmentChat();
+            if (position == 1) {
+                fragment = new FragmentSearch();
 
+            }
+            if (position == 2){
+                fragment = new FragmentChat();
+            }
+            if (position==3){
+                fragment = new FragmentGroups();
+            }
+            if (position==4){
+                fragment = new FragmentSettings();
             }
             return fragment;
         }
 
         @Override
+        public CharSequence getPageTitle(int position) {
+            return tabs[position];
+
+
+        }
+
+        @Override
         public int getCount() {
-            return 2;
+            return 5;
         }
     }
 }
---
+++ app/src/main/java/com/gui/sharemate/viknox/guilevel/FragmentHome.java
@@ -0,0 +1,18 @@
+package com.gui.sharemate.viknox.guilevel;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by x230 on 11/21/2016.
+ */
+public class FragmentHome extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+}
